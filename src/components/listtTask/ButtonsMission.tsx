eaef66236027f2d3a04b1dc597c8e51fcf7e9c25
roee ltmanovich <roeeli770@gmail.com>
2024-10-31 11:01:40 +0200

    create base component for detels

--- src/components/listtTask/ButtonsMission.tsx
+++ src/components/listtTask/ButtonsMission.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+interface Prop {
+    status: string
+}
+
 export default function ButtonsMission() {
   return (
-    <div>ButtonsMission</div>
+    <div>
+        <button>delete</button>
+        <button>Progress</button>
+    </div>
   )
 }
